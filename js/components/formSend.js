215f8809a32784eec77a5cd51d936e5f0fcb5eb0
Ilya Tsygankov <pegas19962010@gmail.com>
2023-06-27 12:08:49 +0300

    validation

--- js/components/formSend.js
+++ js/components/formSend.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 const token = '6235234686:AAHuG42TiUhjZiUbA7sIUK1i_JpujZecv2Y';
 const chatId = '-987994014';
 const urlApi = `https://api.telegram.org/bot${token}/sendMessage`;
@@ -51,10 +50,7 @@ document.querySelector('.feedback__form').addEventListener('submit', function (e
     });
 });
 
-
-
 /* Валидация формы */
-
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const phoneInput = document.querySelector('#phone');
@@ -62,51 +58,70 @@ const namePopup = document.querySelector('.popup-name');
 const emailPopup = document.querySelector('.popup-email');
 const phonePopup = document.querySelector('.popup-phone');
 
-function removeInvalidActive(input) {
-  input.classList.remove('feedback__input--invalid');
-}
-
 function addInvalidActive(input, popup) {
   input.classList.add('feedback__input--invalid');
   popup.classList.add('popup--active');
   setInterval(() => {
     popup.classList.remove('popup--active');
   }, 2500);
-}
+};
+
+function removeInvalidActive(input) {
+  input.classList.remove('feedback__input--invalid');
+};
 
 function nameValidation() {
   const nameRegEx = /[^а-я\s]+/gi;
   const validName = nameInput.value.replace(nameRegEx, "?");
   const checkName = validName.indexOf('?') > -1;
-  if (!checkName) {
-    removeInvalidActive(nameInput);
-    return true
-  } else {
-    addInvalidActive(nameInput, namePopup);
-    return false
-  };
+  checkName || nameInput.value == '' ? addInvalidActive(nameInput, namePopup) : removeInvalidActive(nameInput);
 };
 
 function emailValidation() {
   const emailRegEx = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
   const validEmail = emailRegEx.test(emailInput.value);
-  if (validEmail) {
-    removeInvalidActive(emailInput);
-  } else {
-    addInvalidActive(emailInput, emailPopup);
-  };
+  validEmail && emailInput.value !== '' ? removeInvalidActive(emailInput) : addInvalidActive(emailInput, emailPopup);
 };
 
 function phoneValidation() {
   const phoneRegEx = /\W*_*/g;
   const validPhone = phoneInput.value.replace(phoneRegEx, '');
-  if (validPhone.length == 11) {
-    removeInvalidActive(phoneInput);
+  validPhone.length == 11 ? removeInvalidActive(phoneInput) : addInvalidActive(phoneInput, phonePopup);
+};
+
+function btnValidation() {
+  const inputs = document.querySelectorAll('.feedback__input');
+  const formBtn = document.querySelector('.feedback__button');
+  const inputsStatusArr = [];
+
+  inputs.forEach(input => {
+    const status = input.querySelector('input').classList.contains('feedback__input--invalid');
+    inputsStatusArr.push(status);
+    return inputsStatusArr;
+  });
+
+  if (inputsStatusArr.includes(true)) {
+    formBtn.classList.add('disabled-button');
+    formBtn.setAttribute('disabled', true);
   } else {
-    addInvalidActive(phoneInput, phonePopup);
+    formBtn.classList.remove('disabled-button');
+    formBtn.removeAttribute('disabled')
   };
 };
 
-nameInput.addEventListener('change', nameValidation);
-emailInput.addEventListener('change', emailValidation);
-phoneInput.addEventListener('change', phoneValidation);
+nameValidation();
+emailValidation();
+phoneValidation();
+
+nameInput.addEventListener('change', () => {
+  nameValidation();
+  btnValidation();
+});
+emailInput.addEventListener('change', () => {
+  emailValidation();
+  btnValidation();
+});
+phoneInput.addEventListener('change', () => {
+  phoneValidation();
+  btnValidation();
+});
