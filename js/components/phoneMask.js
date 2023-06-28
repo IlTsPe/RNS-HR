bed9dac7e58b77a82cbbba5c58eec4192172c908
Ilya Tsygankov <pegas19962010@gmail.com>
2023-06-28 11:12:05 +0300

    npm inputMask

--- js/components/phoneMask.js
+++ js/components/phoneMask.js
@@ -1,5 +1,7 @@
 /* Phone mask */
 
+import Inputmask from "inputmask";
+
 const phoneInput = document.querySelector('#phone');
 const inputMask = new Inputmask('+7 (999) 999-99-99');
 inputMask.mask(phoneInput);
